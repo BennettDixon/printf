0da2b1d72eff2d62ab37d748681e6748b8fae31b
BennettDixon <bennettdixon16@gmail.com>
2018-10-21 01:48:42 +0000

    Added descriptions for functions and voided va_list args in get_percent since unused variable

--- char_spec_funcs.c
+++ char_spec_funcs.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * get_char - gets a pointer to an array containing char and null byte
+ * @args: va_list to get argument from of type char
+ *
+ * Return: pointer to array containing char and null byte
+ */
 char *get_char(va_list args)
 {
 	char str[] = { 't', '\0' };
@@ -6,11 +12,24 @@ char *get_char(va_list args)
 	str[0] = va_arg(args, char);
 	return (str);
 }
+/**
+ * get_string - gets a pointer to a string, null terminated
+ * @args: va_list to get argument of type char *
+ *
+ * Return: pointer to beginning of string
+ */
 char *get_string(va_list args)
 {
 	return (va_arg(args, char *));
 }
+/**
+ * get_percent - gets a pointer to a string literal containing "%\0"
+ * @args: va_list required for function pointer, voided, not used
+ *
+ * Return: pointer to string literal containing %
+ */
 char *get_percent(va_list args)
 {
+	void(args);
 	return ("%");
 }
