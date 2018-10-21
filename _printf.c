ad7df33593a2d893d49dad2139bac931270f9021
AlexYu01 <tengalexyu@gmail.com>
2018-10-21 00:33:47 +0000

    Commit printf

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/** _printf - Prints variatic arguments based on format string.
+  * @format: String passed, may contain zero, or more directives.
+  *
+  * Return: Number of characters printed to stdout.
+  */
+
+int _printf(const char *format, ...)
+{
+	char *beg, *end, *temp, busy;
+	int ind, buff_i;
+	va_list args;
+	char *buff;
+
+	if (!format)
+		return (0);
+
+	buff = create_buffer(BUFF_SIZE);
+	va_start(args, format);
+	ind = 0;
+	buff_i = 0;
+	busy = 0;
+
+	while (format[ind])
+	{
+		if (format[ind] == '%' && !busy)
+		{
+			busy = 1;
+		}
+		else if (busy)
+		{
+			if (is_flag(format[ind]))
+			{
+				/* flags */
+			}
+			else if (is_specifier(format[ind]))
+			{
+				temp = get_string_func(format[ind])(args);
+				copy_buff(temp, &buff_i, buff, BUFF_SIZE);
+				busy = 0;
+			}
+			else /* percision */
+			{
+			}
+		}
+		else
+			buff[buff_i++] = format[ind];
+		ind++;
+	}
+
