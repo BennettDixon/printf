611264cc5f42bb748f3a38c692e35542a2eaab35
AlexYu01 <tengalexyu@gmail.com>
2018-10-21 00:33:32 +0000

    Commit header

--- holberton.h
+++ holberton.h
@@ -1,6 +1,8 @@
 #ifndef _PRINTF_H_
 #define _PRINTF_H_
 #include <stdarg.h>
+
+#define BUFF_SIZE 1024;
 /**
  * struct type_specifier - contains a type represented as char
  * @f: print_fucntion for specific type
@@ -15,5 +17,7 @@ int _printf(const char *format, ...);
 char *create_buffer(unsigned int bytes);
 void copy_buff(char *str, int *index, char *buff, unsigned int buff_size);
 void print_buff(char *buff, unsigned int buff_size);
-char *(*get_print_function(char ch))(va_list);
+char *(*get_string_func(char ch))(va_list);
+int is_specifier(char c);
+int is_flag(char c);
 #endif /* _PRINTF_H_ */
