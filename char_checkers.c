12748fd333ce72e9b71f54c6038cbfb2c398033f
AlexYu01 <tengalexyu@gmail.com>
2018-10-21 21:47:35 +0000

    Add unsigned specifier

--- char_checkers.c
+++ char_checkers.c
@@ -11,7 +11,7 @@ int ch_in_array(char c, char *ptr);
 
 int is_specifier(char c)
 {
-	char spec[] = {'c', 's', '%', 'd', 'i', 'b', NULL};
+	char spec[] = {'c', 's', '%', 'd', 'i', 'b', 'u', NULL};
 
 	return (ch_in_array(c, spec));
 }
