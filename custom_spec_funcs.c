6db95aa79b8ebe95eb18f07de9ec929a5f627224
BennettDixon <bennettdixon16@gmail.com>
2018-10-23 01:06:29 +0000

    added custom specifier function that gets the rot13 of a string

--- custom_spec_funcs.c
+++ custom_spec_funcs.c
@@ -26,3 +26,39 @@ char *get_reverse(va_list args)
 
 	return (ret);
 }
+
+/**
+ * get_rot - Gets the Rot13'd copy of the string
+ * @args: va_list containing the string
+ *
+ * Return: char pointer to newly allocated and rot13'd string
+ */
+char *get_rot(va_list args)
+{
+	int index;
+	char is_upper;
+	char *str, *ret;
+
+	str = va_arg(args, unsigned char *);
+	ret = malloc(_strlen + 1);
+	if (!ret)
+		return (NULL);
+	index = 0;
+	while (str[index])
+	{
+		is_upper = (str[index] >= 'A' && str[index] <= 'Z');
+		str[index] += 13;
+		if (is_upper)
+		{
+			if (str[index] > 'Z')
+				str[index] -= 26;
+		}
+		else
+		{
+			if (str[index] > 'z')
+				str[index] -= 26;
+		}
+		index++;
+	}
+	return (ret);
+}
