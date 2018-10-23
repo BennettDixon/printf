13ab4af6636d28ec7648b281cd366a28716d703b
BennettDixon <bennettdixon16@gmail.com>
2018-10-23 18:06:35 +0000

    Added case for different octal cases with same flag

--- get_flag_funcs.c
+++ get_flag_funcs.c
@@ -1,5 +1,11 @@
 #include "holberton.h"
-
+/**
+ * get_flag_func - gets the proper function for a flag
+ * @flag_index: index of flag gotten from _isflag, consistent with struct arr
+ * @spec: specifier to make sure flag supports it
+ *
+ * Return: pointer to a function that takes a char * and returns a char *
+ */
 char *(*get_flag_func(int flag_index, char spec))(char *)
 {
 	char only_ints[] = {'d', 'i', NULL };
@@ -7,10 +13,24 @@ char *(*get_flag_func(int flag_index, char spec))(char *)
 	flag_t flags[] = {
 		{ '+', only_ints, do_plus_flag },
 		{ ' ', only_ints, do_spc_flag },
-		{ '#', only_hex, do_hash_flag },
+		{ '#', only_hex, NULL },
 		{ 0, NULL, NULL },
 		};
 
+	switch (spec)
+	{
+		case ('o'):
+			flags[2].f = do_octal_flag;
+			break;
+		case ('x'):
+			flags[2].f = do_hex_flag;
+			break;
+		case ('X'):
+			flags[2].f = do_hex_upper_flag;
+			break;
+		default:
+			break;
+	}
 	if (ch_in_array(spec, flags[flag_index].specs))
 		return (flags[flag_index].f);
 	return (NULL);
