0cf74ea1342e4f072021d8ccb53f6180a9bdb494
AlexYu01 <tengalexyu@gmail.com>
2018-10-27 03:22:01 +0000

    Move custom specifier functions to custom_spec_funcs file

--- int_spec_funcs.c
+++ int_spec_funcs.c
@@ -47,32 +47,6 @@ char *get_int(va_list args, char *mods)
 }
 
 /**
- * get_binary - convert unsigned integer to binary representation.
- * @args: va_list containing the unsigned integer.
- * @mods: length modifiers, voided, not used with this specifier
- *
- * Return: char pointer to the binary string. NULL if malloc fails.
- */
-
-char *get_binary(va_list args, char *mods)
-{
-	unsigned int num;
-	int length;
-	char *ret;
-
-	(void)mods;
-	num = va_arg(args, unsigned int);
-	length = get_numbase_len(num, 2);
-
-	ret = malloc(length + 1);
-	if (!ret)
-		return (NULL);
-
-	fill_numbase_buff(num, 2, ret, length);
-
-	return (ret);
-}
-/**
  * get_unsigned - gets a string representing an unsigned integer
  * @args: va_list to get unsigned integer from
  * @mods: array containing length modifier bytes, representing t/f for l & h
