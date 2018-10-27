7fac17bf5c1dd4cde7523969cb8d8dd7143199d1
AlexYu01 <tengalexyu@gmail.com>
2018-10-27 03:24:23 +0000

    Move perform flag functions to get_flag_funcs file

--- _printf.c
+++ _printf.c
@@ -235,35 +235,7 @@ printh_t *init_help_s(const char *format)
 	help_s->spec_c = 0;
 	return (help_s);
 }
-/**
- * perform_flag_funcs - Perform flag functions that were encountered on the
- * string.
- * @flags: An array int to keep track of which flags were encountered. 1 if
- * encountered, otherwise 0.
- * @temp: The string to perform functions on.
- * @spec: The specifier.
- *
- * Return: char pointer to the formatted string.
- */
 
-char *perform_flag_funcs(int *flags, char *temp, char spec)
-{
-	int i;
-	char *(*f)(char *);
-
-	for (i = 0; i < 5; i++)
-	{
-		if (flags[i])
-		{
-			f = get_flag_func(i, spec);
-			if (f)
-				temp = f(temp);
-			if (!temp)
-				return (NULL);
-		}
-	}
-	return (temp);
-}
 
 /**
  * exit_busy_reset - Reset values of busy, width, precision, and dot to zero
