6c28668e4e3bf8f711ec658b73fa11b7effb048a
AlexYu01 <tengalexyu@gmail.com>
2018-10-27 04:22:07 +0000

    Cleaning test files

--- testing/flag_tests.c
+++ testing/flag_tests.c
@@ -5,61 +5,91 @@ int main()
 {
 	int len, len2;
 
-	len = _printf("Int: [%+d]", 10);
-	printf("\n");
-	len2 = printf("Int: [%+d]", 10);
-	printf("\n");
+	len = _printf("%%+d: [%+d]\n", 10);
+	len2 = printf("%%+d: [%+d]\n", 10);
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 1: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 
-	len = _printf("Inti: [% i]", 11);
-	printf("\n");
-	len2 = printf("Inti: [% i]", 11);
-	printf("\n");
+	len = _printf("%% i: [% i]\n", 11);
+	len2 = printf("%% i: [% i]\n", 11);
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 2: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 
-	len = _printf("Hash int: [%#d]", 12);
-	printf("\n");
-	len2 = printf("Hash int: [%#d]", 12);
-	printf("\n");
+	len = _printf("%%#d: [%#d]\n", 12);
+	len2 = printf("%%#d: [%#d]\n", 12);
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 3: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("%%#x: [%#x]\n", 13);
+	len2 = printf("%%#x: [%#x]\n", 13);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 4: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
+	len = _printf("%%#x: [%#x]\n", 0);
+	len2 = printf("%%#x: [%#x]\n", 0);
+	printf("len: %d\n", len);
+	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 5: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 
-	len = _printf("Hash hex: [%#x]", 13);
-	printf("\n");
-	len2 = printf("Hash hex: [%#x]", 13);
-	printf("\n");
+	len = _printf("%%#o: [%#o]\n", 14);
+	len2 = printf("%%#o: [%#o]\n", 14);
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 6: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 
-	len = _printf("Hash hex 0: [%#x]", 0);
-	printf("\n");
-	len2 = printf("Hash hex 0: [%#x]", 0);
-	printf("\n");
+	len = _printf("%%#o: [%#o]\n", 0);
+	len2 = printf("%%#o: [%#o]\n", 0);
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 7: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 
-	len = _printf("Hash oct: [%#o]", 14);
-	printf("\n");
-	len2 = printf("Hash oct: [%#o]", 14);
-	printf("\n");
+	len = _printf("%%+x: [%+x]\n", 15);
+	len2 = printf("%%+x: [%+x]\n", 15);
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
-
-	len = _printf("Hash oct 0: [%#o]", 0);
-	printf("\n");
-	len2 = printf("Hash oct 0: [%#o]", 0);
-	printf("\n");
-	printf("len: %d\n", len);
-	printf("len2: %d\n", len2);
-
-	len = _printf("+ hex: [%+x]", 15);
-	printf("\n");
-	len2 = printf("+ hex: [%+x]", 15);
-	printf("\n");
-	printf("len: %d\n", len);
-	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 8: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	printf("----Test width with invalid and/or no specifiers----\n");
 
 
@@ -69,27 +99,36 @@ int main()
 	printf("\n");
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
-
+	if (len != len2)
+	{
+		printf("Test 9: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	len = _printf("% % % % ");
 	printf("\n");
 	len2 = printf("% % % % ");
 	printf("\n");
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
-
+	if (len != len2)
+	{
+		printf("Test 10: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	len = _printf("% ");
 	printf("\n");
 	len2 = printf("% ");
 	printf("\n");
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
-
-	len = _printf("% ");
-	printf("\n");
-	len2 = printf("% ");
-	printf("\n");
-	printf("len: %d\n", len);
-	printf("len2: %d\n", len2);
+	if (len != len2)
+	{
+		printf("Test 11: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 
 	len = _printf("%y %");
 	printf("\n");
@@ -97,17 +136,23 @@ int main()
 	printf("\n");
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
-
+	if (len != len2)
+	{
+		printf("Test 12: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	len = _printf("%y %+");
 	printf("\n");
 	len2 = printf("%y %+");
 	printf("\n");
 	printf("len: %d\n", len);
 	printf("len2: %d\n", len2);
-	
-	
-	
-	
-	
+	if (len != len2)
+	{
+		printf("Test 13: Lengths differ.\n");
+		fflush(stdout);
+		return (1);
+	}
 	return (0);
 }
